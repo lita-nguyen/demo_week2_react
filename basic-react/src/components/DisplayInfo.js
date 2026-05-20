7a17fdee9cdae612f718d4fb53ca8819b1bf1772
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-05-20 10:45:48 +0700

    conditional output

--- basic-react/src/components/DisplayInfo.js
+++ basic-react/src/components/DisplayInfo.js
@@ -1,29 +1,46 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+  state = {
+    isShowListUser: true,
+  };
+
+  handShowHide = () => {
+    this.setState({
+      isShowListUser: !this.state.isShowListUser,
+    });
+  };
+
   render() {
     const { listUsers } = this.props;
     console.log(listUsers);
     return (
       <div>
-        {listUsers.map((user) => {
-          console.log(user);
-          return (
-            <div key={user.id}>
-              <div>My name is {user.name}</div>
-              <div>My age is {user.age}</div>
-              <hr />
-            </div>
-          );
-        })}
-        {/* <div>My name is {name}</div>
-        <div>My age is {age}</div>
-        <hr />
-        <div>My name is {name}</div>
-        <div>My age is {age}</div>
-        <hr />
-        <div>My name is {name}</div>
-        <div>My age is {age}</div> */}
+        <div>
+          <span
+            onClick={() => {
+              this.handShowHide();
+            }}
+          >
+            {this.state.isShowListUser == true
+              ? "Hide list users:"
+              : "Show list users:"}
+          </span>
+        </div>
+
+        {this.state.isShowListUser && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                  <div>My name is {user.name}</div>
+                  <div>My age is {user.age}</div>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
