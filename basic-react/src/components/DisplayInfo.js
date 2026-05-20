1652559368ca2ae65a2fd5e2c7532b646dd8dc0f
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-05-20 13:54:58 +0700

    scss

--- basic-react/src/components/DisplayInfo.js
+++ basic-react/src/components/DisplayInfo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./DisplayInfo.scss";
 
 class DisplayInfo extends React.Component {
   state = {
@@ -15,7 +16,7 @@ class DisplayInfo extends React.Component {
     const { listUsers } = this.props;
     console.log(listUsers);
     return (
-      <div>
+      <div className="display-info-container">
         <div>
           <span
             onClick={() => {
