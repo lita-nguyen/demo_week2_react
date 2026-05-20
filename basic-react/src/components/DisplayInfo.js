0f7d3f7072bfc184439d460800e372f258c95634
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-05-20 21:52:01 +0700

    statefull, stateless

--- basic-react/src/components/DisplayInfo.js
+++ basic-react/src/components/DisplayInfo.js
@@ -2,79 +2,63 @@ import React from "react";
 import "./DisplayInfo.scss";
 import logo from "./../logo.svg";
 
-class DisplayInfo extends React.Component {
-  constructor(props) {
-    console.log("Call constructor: 1");
-    super(props);
-    this.state = {
-      isShowListUser: true,
-    };
-  }
+// class DisplayInfo extends React.Component {
+//   render() {
+//     console.log("Call me render");
+//     const { listUsers } = this.props;
+//     console.log(listUsers);
+//     return (
+//       <div className="display-info-container">
+//         {true && (
+//           <>
+//             {listUsers.map((user) => {
+//               return (
+//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                   <div>My name is {user.name}</div>
+//                   <div>My age is {user.age}</div>
+//                   <div>
+//                     <button
+//                       onClick={() => this.props.handleDeleteUser(user.id)}
+//                     >
+//                       Delete
+//                     </button>
+//                   </div>
+//                   <hr />
+//                 </div>
+//               );
+//             })}
+//           </>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-  componentDidMount() {
-    console.log("Call me component did mount");
-    setTimeout(() => {
-      document.title = "Lita Nguyen";
-    }, 2000);
-  }
+const DisplayInfo = (props) => {
+  const { listUsers } = props;
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log("Call me component did update", this.props, prevProps);
-    if (this.props.listUsers !== prevProps.listUsers) {
-      if (this.props.listUsers.length === 5) {
-        alert("You got 5 users");
-      }
-    }
-  }
-
-  handShowHide = () => {
-    this.setState({
-      isShowListUser: !this.state.isShowListUser,
-    });
-  };
-
-  render() {
-    console.log("Call me render");
-    const { listUsers } = this.props;
-    console.log(listUsers);
-    return (
-      <div className="display-info-container">
-        <img src={logo} />
-        <div>
-          <span
-            onClick={() => {
-              this.handShowHide();
-            }}
-          >
-            {this.state.isShowListUser == true
-              ? "Hide list users:"
-              : "Show list users:"}
-          </span>
-        </div>
-
-        {this.state.isShowListUser && (
-          <>
-            {listUsers.map((user) => {
-              return (
-                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  <div>My name is {user.name}</div>
-                  <div>My age is {user.age}</div>
-                  <div>
-                    <button
-                      onClick={() => this.props.handleDeleteUser(user.id)}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                  <hr />
+  return (
+    <div className="display-info-container">
+      {true && (
+        <>
+          {listUsers.map((user) => {
+            return (
+              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                <div>My name is {user.name}</div>
+                <div>My age is {user.age}</div>
+                <div>
+                  <button onClick={() => props.handleDeleteUser(user.id)}>
+                    Delete
+                  </button>
                 </div>
-              );
-            })}
-          </>
-        )}
-      </div>
-    );
-  }
-}
+                <hr />
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfo;
