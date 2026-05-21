90c95717ebb278570a91c9a8b842ba8c32534969
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-05-21 12:57:01 +0700

    setup bootstrap and react router dom

--- basic-react/src/App.js
+++ basic-react/src/App.js
@@ -1,17 +1,7 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import MyComponent from "./components/MyComponent";
-import React from "react";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        Hello World
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <div className="app-container">Hello World</div>;
+};
 
 export default App;
